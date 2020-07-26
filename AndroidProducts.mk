3d66e8e126546093505dd7f5a1408e22c8475289
Arth Doshi <55782346+arthdoshi33@users.noreply.github.com>
2020-07-26 13:54:33 +0000

    santoni: Update lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/revengeos_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_santoni-userdebug
+    revengeos_santoni-userdebug \
+    revengeos_santoni-user \
+    revengeos_santoni-eng
