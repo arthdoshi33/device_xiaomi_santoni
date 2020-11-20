75b8c2744091d1ac8d28c2e9dbce2ee1c8bbb0a3
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-11-20 07:54:59 +0000

    santoni: Enable freeform windows

--- device.mk
+++ device.mk
@@ -162,6 +162,10 @@ PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8937 \
